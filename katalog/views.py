1cd8add8e84e6b6e4276272b9c3ecbd28e4d547b
ratuanvilen <ratu.almas@ui.ac.id>
2022-09-14 21:46:02 -0700

    tugas

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem 
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang_katalog' : data_katalog,
+        'nama' : 'Ratu Almas Naurah Anvilen',
+        'NPM' : '2106752035',
+    }
+    return render(request, "katalog.html", context)
--- project_django/urls.py
+++ project_django/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
+    path('katalog/', include('katalog.urls'))
 ]
 
 # TODO: Implement Routings Here
